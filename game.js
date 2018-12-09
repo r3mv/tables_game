fbfca063176251ba81be0a7fa851876213d0daca
= <remi.vieux@gmail.com>
2018-12-09 23:33:36 +0100

    upd:Fix javascript syntax mistakes. A game can be created and a game loop is hard coded. Timeouts are failing

--- game.js
+++ game.js
@@ -1,101 +1,133 @@
-class game {
-    constructor () {
-        
-    }
-}
+function Game(context) {
 
-game.tables = [1,2,3,4,5,6,7,8,9,10];
-game.operator = "x";
-game.max = 10;
-
-game.Levels = Object.freeze({"beginner":1, "medium":2, "expert":3, "computer":4});
-
-game.Operators = Oject.freeze({
-        addition : {
-            toString : "+"
+    this.Levels = Object.freeze({ "beginner": 1, "medium": 2, "expert": 3, "computer": 4 });
+    this.Operators = Object.freeze({
+        addition: {
+            toString: "+"
         },
-        substraction : {
-            toString : "-"
+        substraction: {
+            toString: "-"
         },
-        multiplication : {
-            toString : "x"
+        multiplication: {
+            toString: "x"
         },
-        division : {
-            toString : "/"
+        division: {
+            toString: "/"
         }
-    } 
-);
-
-game.level = game.Levels.beginner;
-
-game.responseDelay = 10;
-
-game.setOperator = function(operator)
-{
-    this.operator = operator;
-}
-
-game.setLevel = function(level)
-{
-    this.level = level;
-}
-
-game.setMax = function(max) {
-    this.max = max;
-}
-
-// set an array of tables to compute
-game.setTables = function(tables) {
-    this.tables = tables;
-}
-
-game.init = function() {
-    switch(game.level) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
     }
-}
-
-game.handlers = [] ;
-
-/*
-* Add a GUI game handler, i.e. a component with callback functions
-* that can be called from this game to display the current game
-* status.
-* handler must implement:
-* newItem (item { x : val, y : val}, operator) -> call when a new operation is submitted to the game
-*/
-game.addHandler = function(guiHandler) {
-    this.handlers.push(guiHandler);
-}
-game.removeHandler = function(guiHandler) {
-    this.handlers = this.handlers.filter(
-        function (item) {
-            if (item !== guiHandler) {
-                return item;
-            }
-        }
     );
-}
 
-game.items = [
-    { x: 1, y: 1}, {x:1, y:2}, {x2:3, y:52}
-];
+    this.tables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    this.operator = "x";
+    this.max = 10;
+    this.level = this.Levels.beginner;
+    this.responseDelay_ms = 1000;
+    this.handlers = [];
+    this.items = [
+        { x: 1, y: 1 }, { x: 1, y: 2 }, { x: 3, y: 52 }
+    ];
+    this.currentTimeout = null;
 
-game.start = function() {
-    while (game.items.length) {
-        var item = game.items.pop();
-        this.fireNewItem(item);
-        this.currentTimeout = setTimeout(this.fireTimeout(), 500);
+    this.setOperator = function (operator) {
+        this.operator = operator;
     }
-}
 
-game.currentTimeout = null;
-game.fireNewItem = function (item, thisObj) {
-    var scope = thisObj || window;
-    this.handlers.forEach(function(handler) {
-        handler.newItem.call(scope, item, this.operator.toString);
-    });
+    this.setLevel = function (level) {
+        this.level = level;
+    }
+
+    this.setMax = function (max) {
+        this.max = max;
+    }
+
+    // set an array of tables to compute
+    this.setTables = function (tables) {
+        this.tables = tables;
+    }
+
+    this.init = function () {
+        switch (game.level) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+        }
+    }
+
+    /*
+    * Add a GUI game handler, i.e. a component with callback functions
+    * that can be called from this game to display the current game
+    * status.
+    * handler must implement:
+    * newItem: (item { x : val, y : val}, operator) -> call when a new operation is submitted to the game
+    * hasTimedOut: function called when timeout is reached for giving an answer
+    * endofgame: function calld when the game is finished
+    */
+    this.addHandler = function (guiHandler) {
+        this.handlers.push(guiHandler);
+    }
+
+    this.removeHandler = function (guiHandler) {
+        this.handlers = this.handlers.filter(
+            function (item) {
+                if (item !== guiHandler) {
+                    return item;
+                }
+            }
+        );
+    }
+
+
+    this.start = function () {
+        this.loop();
+    }
+
+    this.loop = function (item) {
+        if (this.currentTimeout !== null) {
+            console.log("Clearing timeout")
+            window.clearTimeout(this.currentTimeout);
+            this.currentTimeout = null;
+        }
+        var item = this.items.pop();
+        if (item !== undefined) {
+            this.fireNewItem(item);
+            this.currentTimeout = window.setTimeout(this.fireTimeout(this), 5000);
+            console.log("Setting timeout for " + item + " to 5000 ms");
+        } else {
+            this.fireEndOfGame(this);
+        }
+    }
+
+    this.fireNewItem = function (item, thisObj) {
+        var scope = thisObj || window;
+        var operator = this.operator.toString();
+        this.handlers.forEach(function (handler) {
+            if (handler.newItem !== undefined) {
+                handler.newItem.call(scope, item, operator);
+            }
+        });
+    }
+
+    this.fireTimeout = function (thisObj) {
+        this.handlers.forEach(function (handler) {
+            var scope = thisObj || window;
+            if (handler.hasTimedOut !== undefined) {
+                handler.hasTimedOut.call(scope);
+
+            }
+        });
+        // TODO notify handles that timeout has reached
+        // TODO notify game that this item is failed
+        this.loop();
+    }
+
+    this.fireEndOfGame = function(thisObj) {
+        this.handlers.forEach(function (handler) {
+            var scope = thisObj || window;
+            if (handler.endOfGame !== undefined) {
+                handler.endOfGame.call(scope);
+            }
+        });
+    }
+
 }
