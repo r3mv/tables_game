f17b982e7e6d57031f4b671e16b8139fa30aceea
= <remi.vieux@gmail.com>
2018-12-09 21:57:50 +0100

    upd:continuing implementation of game engine. trivial mistakes are throwing errors

--- game.js
+++ game.js
@@ -1,10 +1,31 @@
-var game = game || {} ; 
+class game {
+    constructor () {
+        
+    }
+}
 
 game.tables = [1,2,3,4,5,6,7,8,9,10];
 game.operator = "x";
 game.max = 10;
 
 game.Levels = Object.freeze({"beginner":1, "medium":2, "expert":3, "computer":4});
+
+game.Operators = Oject.freeze({
+        addition : {
+            toString : "+"
+        },
+        substraction : {
+            toString : "-"
+        },
+        multiplication : {
+            toString : "x"
+        },
+        division : {
+            toString : "/"
+        }
+    } 
+);
+
 game.level = game.Levels.beginner;
 
 game.responseDelay = 10;
@@ -29,5 +50,52 @@ game.setTables = function(tables) {
 }
 
 game.init = function() {
-    
+    switch(game.level) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+    }
+}
+
+game.handlers = [] ;
+
+/*
+* Add a GUI game handler, i.e. a component with callback functions
+* that can be called from this game to display the current game
+* status.
+* handler must implement:
+* newItem (item { x : val, y : val}, operator) -> call when a new operation is submitted to the game
+*/
+game.addHandler = function(guiHandler) {
+    this.handlers.push(guiHandler);
+}
+game.removeHandler = function(guiHandler) {
+    this.handlers = this.handlers.filter(
+        function (item) {
+            if (item !== guiHandler) {
+                return item;
+            }
+        }
+    );
+}
+
+game.items = [
+    { x: 1, y: 1}, {x:1, y:2}, {x2:3, y:52}
+];
+
+game.start = function() {
+    while (game.items.length) {
+        var item = game.items.pop();
+        this.fireNewItem(item);
+        this.currentTimeout = setTimeout(this.fireTimeout(), 500);
+    }
+}
+
+game.currentTimeout = null;
+game.fireNewItem = function (item, thisObj) {
+    var scope = thisObj || window;
+    this.handlers.forEach(function(handler) {
+        handler.newItem.call(scope, item, this.operator.toString);
+    });
 }
